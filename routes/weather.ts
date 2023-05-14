59755f8ec63f9cda9f41416a9d4fbd1bd5bd3774
kolestom <88550454+kolestom@users.noreply.github.com>
2023-05-14 17:33:41 +0200

    weather tycatch added

--- routes/weather.ts
+++ routes/weather.ts
@@ -12,10 +12,16 @@ const WeatherReqSchema = z.object({
 
 
 router.post("/", verifyReqSchema(WeatherReqSchema), async(req: Request, res: Response) =>{
-    const apiResponse = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${env.WEATHER_API_KEY}&q=${req.body.city}+${req.body.country}&days=5&lang=en`)
-
-    if (!apiResponse) return res.sendStatus(502)
-    res.send(apiResponse.data)  
+    try {
+        
+        const apiResponse = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${env.WEATHER_API_KEY}&q=${req.body.city}+${req.body.country}&days=5&lang=en`)
+        
+        // if (!apiResponse) return res.sendStatus(502)
+        res.send(apiResponse.data)  
+    } catch (error: any) {
+        console.log(error.message);
+        res.sendStatus(502)
+    }
 })
 
 export default router
